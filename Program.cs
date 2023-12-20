daeeebd0b77d4d349d3ae97f109022ae6bdb5766
JoabTorress <joabtorres28@gmail.com>
2023-12-20 11:30:30 -0300

    segundo commit

--- Program.cs
+++ Program.cs
@@ -4,9 +4,60 @@ namespace Ti360
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.Write("Digite seu peso em quilogramas: ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite sua altura em metros: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            
+            double imc = CalcularIMC(peso, altura);
+
+            
+            Console.WriteLine($"Seu IMC é: {imc:F2}");
+
+            
+            ExibirClassificacaoIMC(imc);
+        }
+
+       
+        static double CalcularIMC(double peso, double altura)
+        {
+            return peso / (altura * altura);
+        }
+
+        static void ExibirClassificacaoIMC(double imc)
+        {
+            Console.Write("Classificação do IMC: ");
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (imc < 24.9)
+            {
+                Console.WriteLine("Peso normal");
+            }
+            else if (imc < 29.9)
+            {
+                Console.WriteLine("Sobrepeso");
+            }
+            else if (imc < 34.9)
+            {
+                Console.WriteLine("Obesidade Grau I");
+            }
+            else if (imc < 39.9)
+            {
+                Console.WriteLine("Obesidade Grau II");
+            }
+            else
+            {
+                Console.WriteLine("Obesidade Grau III");
+            }
         }
     }
 }
+
